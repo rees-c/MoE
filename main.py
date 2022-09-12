7b7b145b8598e5d6b4ac9ed65ac37cc59e8b4177
rees-c <rees.chang@gmail.com>
2022-09-12 02:35:49 -0500

    bn eval

--- main.py
+++ main.py
@@ -295,7 +295,7 @@ def main(dataset_name='expt_eform', n_head_layers=3,
         str(seed) + '_best_model.pth'
 
     early_stopping_n_epochs = 500
-    for epoch in range(2):  # 1000 epochs
+    for epoch in range(1000):  # 1000 epochs
         for structures, labels, _ in train_dl:
             train(structures, labels, model, cuda, option, normalizer,
                   train_loss_meter, optimizer, extractors, ensembled_backbone,
